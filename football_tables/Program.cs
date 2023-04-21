02b5ded0a70b2128fd31dca98bba6a8e61d86a4f
OliverBark <oliver.bark@icloud.com>
2023-04-21 13:45:18 +0200

    Fikset fejl

--- football_tables/Program.cs
+++ football_tables/Program.cs
@@ -8,7 +8,7 @@ class Program
     static void Main(string[] args)
     {
         string teamsFile = "csv_files/teams/teams.csv"; // File path for teams.csv
-        List<Team> teams = ReadTeamsFromCsv(teamsFile); // Read team names from teams.csv
+        List<string> teams = ReadTeamsFromCsv(teamsFile); // Read team names from teams.csv
 
         string setupFile = "csv_files/setup/setup.csv"; // Updated file path
 
@@ -50,10 +50,6 @@ class Program
                 List<string> fixtures = GenerateFixtures(teams);
                 WriteCsvFile(leagueName, i, fixtures, $"round{i}.csv");
             }
-
-            // Generate and display league table
-            List<Team> leagueTable = GenerateLeagueTable(teams);
-            DisplayLeagueTable(leagueTable);
         }
         else
         {
@@ -61,30 +57,19 @@ class Program
         }
     }
 
-    static List<Team> ReadTeamsFromCsv(string teamsFile)
+    static List<string> ReadTeamsFromCsv(string teamsFile)
     {
-        List<Team> teams = new List<Team>();
+        List<string> teams = new List<string>();
         string[] lines = File.ReadAllLines(teamsFile, Encoding.UTF8);
         for (int i = 1; i < lines.Length; i++)
         {
             string[] fields = lines[i].Split(',');
-            Team team = new Team(fields[1])
-            {
-
-                GamesPlayed = 0,
-                GamesWon = 0,
-                GamesDrawn = 0,
-                GamesLost = 0,
-                GoalsFor = 0,
-                GoalsAgainst = 0,
-                CurrentStreak = "-"
-            };
-            teams.Add(team);
+            teams.Add(fields[0]);
         }
         return teams;
     }
 
-    static List<string> GenerateFixtures(List<Team> teams)
+    static List<string> GenerateFixtures(List<string> teams)
     {
         List<string> fixtures = new List<string>();
         Random random = new Random();
@@ -92,144 +77,39 @@ class Program
         // Shuffle teams
         for (int i = teams.Count - 1; i > 0; i--)
         {
-                            int j = random.Next(0, i + 1);
-                Team temp = teams[i];
-                teams[i] = teams[j];
-                teams[j] = temp;
-            }
+            int j = random.Next(0, i + 1);
+            string temp = teams[i];
+            teams[i] = teams[j];
+            teams[j] = temp;
+        }
 
-        // Generate fixtures
-        for (int i = 0; i < teams.Count - 1; i++)
+        for (int i = 0; i < teams.Count; i += 2)
         {
-            for (int j = i + 1; j < teams.Count; j++)
-            {
-                string fixture = $"{teams[i].Name},{teams[j].Name}";
-                fixtures.Add(fixture);
-            }
+            string fixture = $"{teams[i]},{teams[i + 1]},{random.Next(0, 5)},{random.Next(0, 5)}";
+            fixtures.Add(fixture);
         }
 
         return fixtures;
     }
 
-    static void WriteCsvFile(string leagueName, int round, List<string> fixtures, string fileName)
+    static void WriteCsvFile(
+        string leagueName,
+        int roundNumber,
+        List<string> fixtures,
+        string fileName
+    )
     {
-        string[] lines = new string[fixtures.Count + 1];
-        lines[0] = "Fixture";
-        for (int i = 0; i < fixtures.Count; i++)
-        {
-            lines[i + 1] = fixtures[i];
-        }
-        File.WriteAllLines($"csv_files/rounds/{fileName}", lines, Encoding.UTF8);
-    }
+        string filePath = $"csv_files/rounds/{fileName}";
 
-    static List<Team> GenerateLeagueTable(List<Team> teams)
-    {
-        List<Team> leagueTable = new List<Team>(teams);
-        leagueTable.Sort((x, y) =>
+        using (StreamWriter writer = new StreamWriter(filePath))
         {
-            int pointsComparison = y.Points.CompareTo(x.Points);
-            if (pointsComparison != 0)
+            writer.WriteLine($"HomeTeam,AwayTeam,HomeScore,AwayScore");
+            foreach (string fixture in fixtures)
             {
-                return pointsComparison;
+                writer.WriteLine(fixture);
             }
-            else
-            {
-                int goalDifferenceComparison = y.GoalDifference.CompareTo(x.GoalDifference);
-                if (goalDifferenceComparison != 0)
-                {
-                    return goalDifferenceComparison;
-                }
-                else
-                {
-                    int goalsForComparison = y.GoalsFor.CompareTo(x.GoalsFor);
-                    if (goalsForComparison != 0)
-                    {
-                        return goalsForComparison;
-                    }
-                    else
-                    {
-                        return x.Name.CompareTo(y.Name);
-                    }
-                }
-            }
-        });
-        return leagueTable;
-    }
-
-    static void DisplayLeagueTable(List<Team> leagueTable)
-    {
-        Console.WriteLine();
-        Console.WriteLine("{0,-25}{1,-10}{2,-10}{3,-10}{4,-10}{5,-10}{6,-10}{7,-10}",
-            "Team", "P", "W", "D", "L", "GF", "GA", "PTS");
-        foreach (var team in leagueTable)
-        {
-            Console.WriteLine("{0,-25}{1,-10}{2,-10}{3,-10}{4,-10}{5,-10}{6,-10}{7,-10}",
-                team.Name, team.GamesPlayed, team.GamesWon, team.GamesDrawn,
-                team.GamesLost, team.GoalsFor, team.GoalsAgainst, team.Points);
         }
+
+        Console.WriteLine("Round {0} fixtures generated and written to {1}", roundNumber, fileName);
     }
 }
-
-class Team
-{
-    public string Name { get; set; }
-    public int GamesPlayed { get; set; }
-    public int GamesWon { get; set; }
-    public int GamesDrawn { get; set; }
-    public int GamesLost { get; set; }
-    public int GoalsFor { get; set; }
-    public int GoalsAgainst { get; set; }
-    public int GoalDifference => GoalsFor - GoalsAgainst;
-    public int Points => GamesWon * 3 + GamesDrawn;
-    public string CurrentStreak { get; set; }
-
-    // Constructor
-    public Team(string name)
-    {
-        Name = name;
-        GamesPlayed = 0;
-        GamesWon = 0;
-        GamesDrawn = 0;
-        GamesLost = 0;
-        GoalsFor = 0;
-        GoalsAgainst = 0;
-        CurrentStreak = "-";
-    }
-
-    // Method to update team statistics
-    public void UpdateStatistics(int goalsFor, int goalsAgainst, string result)
-    {
-        GamesPlayed++;
-        GoalsFor += goalsFor;
-        GoalsAgainst += goalsAgainst;
-
-        switch (result)
-        {
-            case "W":
-                GamesWon++;
-                CurrentStreak = CurrentStreak == "L" ? "W" : CurrentStreak + "W";
-                break;
-            case "D":
-                GamesDrawn++;
-                CurrentStreak = CurrentStreak == "L" ? "D" : CurrentStreak + "D";
-                break;
-            case "L":
-                GamesLost++;
-                CurrentStreak = CurrentStreak == "W" ? "L" : CurrentStreak + "L";
-                break;
-        }
-    }
-
-    // Method to reset team statistics
-    public void ResetStatistics()
-    {
-        GamesPlayed = 0;
-        GamesWon = 0;
-        GamesDrawn = 0;
-        GamesLost = 0;
-        GoalsFor = 0;
-        GoalsAgainst = 0;
-        CurrentStreak = "-";
-    }
-}
-
